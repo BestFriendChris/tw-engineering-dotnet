692110771a7b40947bba95382704f96f9692900e
unknown <shwetaj@.(none)>
2011-06-09 19:00:35 +0100

    [Shweta] Added admin controller

---
+++ UnitTests/Controllers/AdminControllerTests.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using Moq;
+using MvcContrib.TestHelper;
+using NUnit.Framework;
+using VideoWorld.Controllers;
+using VideoWorld.Models;
+using VideoWorld.Repositories;
+
+namespace UnitTests.Controllers
+{
+    class AdminControllerTests
+    {
+        private Mock<ICustomerRepository> mockedCustomerRepository;
+        private AdminController controller;
+
+        [SetUp]
+        public void SetUp()
+        {
+            mockedCustomerRepository =new Mock<ICustomerRepository>();
+            var builder = new TestControllerBuilder();
+            controller = builder.CreateController<AdminController>(mockedCustomerRepository.Object);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            mockedCustomerRepository.VerifyAll();
+        }
+
+        [Test]
+        public void ShouldShowAdminPage()
+        {
+            var customers = new List<Customer> {new Customer(), new Customer()};
+            mockedCustomerRepository.Setup(repository => repository.SelectAllInAlphabeticalOrder()).Returns(customers);
+            var viewResult = controller.Index();
+            viewResult.AssertViewRendered().ForView("Index");
+            Assert.That(viewResult.Model is List<Customer>);
+            Assert.That(((List<Customer>)viewResult.Model).Count, Is.EqualTo(customers.Count));
+        }
+    }
+}
---
+++ VideoWorld/Controllers/AdminController.cs
@@ -0,0 +1,21 @@
+﻿using System.Web;
+using System.Web.Mvc;
+using VideoWorld.Repositories;
+
+namespace VideoWorld.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly ICustomerRepository customerRepository;
+
+        public AdminController(ICustomerRepository customerRepository)
+        {
+            this.customerRepository = customerRepository;
+        }
+
+        public ViewResult Index()
+        {
+            return View("Index", customerRepository.SelectAllInAlphabeticalOrder());
+        }
+    }
+}
